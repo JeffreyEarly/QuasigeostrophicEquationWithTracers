32db8404480166b14ebe5ac7101bf5d373561dcf
Jeffrey J. Early <jearly@nwra.com>
2013-10-08 09:47:05 -0700

    Initial Commit

---
+++ QuasigeostrophicEquationWithTracers/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  QuasigeostrophicEquationWithTracers
+//
+//  Created by Jeffrey J. Early on 10/8/13.
+//  Copyright (c) 2013 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
